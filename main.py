128f048a0f8ca6b00858c3eb294674156dd8624d
Thorsten Kober <Thorsten.Kober@googlemail.com>
2021-08-29 00:41:35 +0200

    Update main.py
    Adopted new WindowsTheme class

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
 import winreg
 
-from windowsdwm import WindowsDwm
+from windowstheme import WindowsTheme
 
 if __name__ == '__main__':
-    dwm = WindowsDwm()
-    print(dwm.getAccentColor())
-    print(dwm.getAccentColorInactive())
-    print(dwm.getColorPrevalence())
+    theme = WindowsTheme()
 
+    theme.setDwmAccentColor(4288052579)
+    theme.setDwmAccentColorInactive(4288052579)
+    theme.setDwmColorPrevalence(1)
+    theme.setExplorerAccentColor(4288052579)
+
+    print(theme.getDwmAccentColor())
+    print(theme.getDwmAccentColorInactive())
+    print(theme.getDwmColorPrevalence())
+    print(theme.getExplorerAccentColor())
+
+    # A320
     #4288052579
     #4288052579
-    #1
+    #True
+
+    # Normal
+    #4279654135
+    #4288052579
+    #False
+    #4279654135
