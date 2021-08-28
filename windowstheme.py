a50e0d07bd3bb84973ba194eb359e81f7d59a59e
Thorsten Kober <Thorsten.Kober@googlemail.com>
2021-08-29 01:45:50 +0200

    Update windowstheme.py
    Themes can now be loaded

--- windowstheme.py
+++ windowstheme.py
@@ -1,4 +1,32 @@
 import winreg
+from dataclasses import dataclass
+
+
+@dataclass
+class Theme:
+    dwmAccentColor: int
+    dwmAccentColorInactive: int
+    dwmColorPrevalence: bool
+    explorerAccentColorMenu: int
+
+
+@dataclass()
+class ThemeColor:
+    r: int
+    g: int
+    b: int
+
+    def toRegDword(self) -> int:
+        return (0xff<<24) + (self.b<<16) + (self.g<<8) + self.r
+
+    def fromRegDword(value: int):
+        bytes = value.to_bytes(4, byteorder='little')
+        r = bytes[0]
+        g = bytes[1]
+        b = bytes[2]
+
+        return ThemeColor(r=r, g=g, b=b)
+
 
 class WindowsTheme:
 
@@ -23,6 +51,12 @@ class WindowsTheme:
         keyh = winreg.OpenKey(self.__registryHkey, hSubKey, access=winreg.KEY_SET_VALUE)
         winreg.SetValueEx(keyh, valueName, 0, winreg.REG_DWORD, value)
         keyh.Close()
+
+    def loadTheme(self, theme: Theme):
+        self.setDwmAccentColor(theme.dwmAccentColor)
+        self.setDwmAccentColorInactive(theme.dwmAccentColorInactive)
+        self.setDwmColorPrevalence(theme.dwmColorPrevalence)
+        self.setExplorerAccentColor(theme.explorerAccentColorMenu)
 
     # Accent Color
     def getDwmAccentColor(self) -> int:
