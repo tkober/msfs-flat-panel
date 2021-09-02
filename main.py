bb8c5b017d0fd83f20e7355088ca3b393b0916d6
Thorsten Kober <Thorsten.Kober@googlemail.com>
2021-09-03 00:09:35 +0200

    Update main.py
    Added option argument to restore the saved original Windows theme

--- main.py
+++ main.py
@@ -3,19 +3,18 @@ import time
 from windowstheme import WindowsThemeInterface, Theme
 import signal
 import sys
+import argparse
 
 ORIGINAL_THEME_PATH = '.tmp\\original_theme.json'
 
 def onSigInt(sig, frame):
-    theme = Theme.fromFile(ORIGINAL_THEME_PATH)
-    WindowsThemeInterface().loadTheme(theme)
-    print('Restored original theme')
+    restoreOriginalTheme()
     sys.exit(0)
 
 def saveCurrentTheme(path: str):
     WindowsThemeInterface().currentTheme().toFile(path)
 
-def main():
+def loadA320():
     # Save current theme
     saveCurrentTheme(ORIGINAL_THEME_PATH)
     print('Saved current theme')
@@ -30,5 +29,33 @@ def main():
         time.sleep(1)
 
 
+def parseArgs():
+    argparser = argparse.ArgumentParser(
+        prog='msfs-flat-panel',
+        description='Sets up your monitor for a flat panel flight sim.'
+    )
+
+    argparser.add_argument(
+        '-r',
+        '--restore',
+        help = "Restores last stored original Windows theme.",
+        action = "store_true"
+    )
+
+    return argparser.parse_args()
+
+
+def restoreOriginalTheme():
+    theme = Theme.fromFile(ORIGINAL_THEME_PATH)
+    WindowsThemeInterface().loadTheme(theme)
+    print('Restored original theme')
+
+
 if __name__ == '__main__':
-    main()
+    args = parseArgs()
+
+    if args.restore:
+        restoreOriginalTheme()
+
+    else:
+        loadA320()
